ec723c161e123d47118bbdd13bf37349cde4222d
Edwin Vera <edwinvera@Edwins-MacBook-Air.local>
2020-07-06 10:41:21 -0400

    final III

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -5,42 +5,42 @@ export default function Explore() {
     <div className="footer-links">
       <div className= "footer1">
         <div>
-          <a href="https://www.smashbros.com/en_US/" style={{ textDecoration: "none" }}>Super Smash Bros.</a>
+          <a href="https://www.smashbros.com/en_US/" target="_blank" style={{ textDecoration: "none" }}>Super Smash Bros.</a>
         </div>
         <div>
-          <a href="https://mario.nintendo.com/" style={{ textDecoration: "none" }}>Super Mario Bros.</a>
+          <a href="https://mario.nintendo.com/" target="_blank" style={{ textDecoration: "none" }}>Super Mario Bros.</a>
         </div>
         <div>
-          <a href="https://www.zelda.com/" style={{ textDecoration: "none" }}>Legend of Zelda</a>
+          <a href="https://www.zelda.com/" target="_blank" style={{ textDecoration: "none" }}>Legend of Zelda</a>
         </div>
         <div>
-          <a href="https://animal-crossing.com/" style={{ textDecoration: "none" }}>Animal Crossing</a>
+          <a href="https://animal-crossing.com/" target="_blank" style={{ textDecoration: "none" }}>Animal Crossing</a>
         </div>
         <div>
-          <a href="https://www.nintendo.com/games/detail/metroid-samus-returns-3ds/" style={{ textDecoration: "none" }}>Metroid</a>
+          <a href="https://www.nintendo.com/games/detail/metroid-samus-returns-3ds/" target="_blank" style={{ textDecoration: "none" }}>Metroid</a>
         </div>
         <div>
-          <a href="https://splatoon.nintendo.com/" style={{ textDecoration: "none" }}>Splatoon</a>
+          <a href="https://splatoon.nintendo.com/" target="_blank" style={{ textDecoration: "none" }}>Splatoon</a>
         </div>
       </div>
       <div className= "footer2">
         <div>
-          <a href="https://www.pokemon.com/us/" style={{ textDecoration: "none" }}>Pokemon</a>
+          <a href="https://www.pokemon.com/us/" target="_blank" style={{ textDecoration: "none" }}>Pokemon</a>
         </div>
         <div>
-          <a href="https://kirby.nintendo.com/" style={{ textDecoration: "none" }}>Kirby</a>
+          <a href="https://kirby.nintendo.com/" target="_blank" style={{ textDecoration: "none" }}>Kirby</a>
         </div>
         <div>
-          <a href="https://fireemblem.nintendo.com/three-houses/" style={{ textDecoration: "none" }}>Fire Emblem</a>
+          <a href="https://fireemblem.nintendo.com/three-houses/" target="_blank" style={{ textDecoration: "none" }}>Fire Emblem</a>
         </div>
         <div>
-          <a href="http://megaman.capcom.com/" style={{ textDecoration: "none" }}>Mega Man</a>
+          <a href="http://megaman.capcom.com/" target="_blank" style={{ textDecoration: "none" }}>Mega Man</a>
         </div>
         <div>
-          <a href="https://yachtclubgames.com/shovel-knight/" style={{ textDecoration: "none" }}>Shovel Knight</a>
+          <a href="https://yachtclubgames.com/shovel-knight/" target="_blank" style={{ textDecoration: "none" }}>Shovel Knight</a>
         </div>
         <div>
-          <a href="https://www.nintendo.com/amiibo/line-up/" style={{ textDecoration: "none" }}>The Entire Character Lineup</a>
+          <a href="https://www.nintendo.com/amiibo/line-up/" target="_blank" style={{ textDecoration: "none" }}>The Entire Character Lineup</a>
         </div>
       </div>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className= "nav-links">
-      <a href= "https://www.nintendo.com/" style={{color: "white", textDecoration: "none"}}>Official Site</a>
+      <a href= "https://www.nintendo.com/" target="_blank" style={{color: "white", textDecoration: "none"}}>Official Site</a>
       <br/>
-      <a href="https://www.nintendo.com/amiibo/buynow/" style={{ color: "white", textDecoration: "none" }}>Buy</a>
+      <a href="https://www.nintendo.com/amiibo/buynow/" target="_blank" style={{ color: "white", textDecoration: "none" }}>Buy</a>
     </div>
   )
 }
